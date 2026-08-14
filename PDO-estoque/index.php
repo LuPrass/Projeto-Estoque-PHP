c4306270a0ecb98ff714307d6d2852193e4200e5
LuPrass <lu.rprass@gmail.com>
2026-08-14 09:43:55 -0300

    Controllers

--- PDO-estoque/index.php
+++ PDO-estoque/index.php
@@ -1,3 +1,19 @@
+<?php
+require_once __DIR__ .'/config/conexao.php';
+require_once __DIR__ .'/src/controllers/ClienteControllers.php';
+$clienteController = new ClienteController($pdo);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST['create'])){
+        $clienteController->create($_POST['name'], $_POST['email'], $_POST['cpf']);
+    }elseif(isset($_POST['update'])){
+       
+    }elseif(isset($_POST['delete'])){
+        
+    }
+}
+$clientes = $clienteController->index();
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -5,14 +21,24 @@
     <title>Painel do Sistema de Estoque</title>
 </head>
 <body>
-    <h1>Painel de Navegação - Sistema de Estoque</h1>
-    <ul>
-        <li><a href="forms/form_usuario.php">Cadastrar Usuário</a></li>
-        <li><a href="forms/form_categoria.php">Cadastrar Categoria</a></li>
-        <li><a href="forms/form_fornecedor.php">Cadastrar Fornecedor</a></li>
-        <li><a href="forms/form_produto.php">Cadastrar Produto</a></li>
-        <li><a href="forms/form_tipo.php">Cadastrar Tipo de Movimentação</a></li>
-        <li><a href="forms/form_movimentacao.php">Registrar Movimentação (N..N)</a></li>
-    </ul>
+    <h1>Clientes</h1>
+    <form id="createCliente" method="POST">
+        <input type="text" id="name" name="name" placeholder="Nome">
+        <input type="email" id="email" name="email" placeholder="Email">
+        <input type="text" id="cpf" name="cpf" placeholder="CPF">
+        <button type="submit">Cadastrar</button>
+    </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+    </table>
 </body>
 </html>
--- PDO-estoque/src/controllers/ClienteControllers.php
+++ PDO-estoque/src/controllers/ClienteControllers.php
@@ -7,7 +7,20 @@ class UserController{
     public function __construct($pdo){
         $this->cliente = new Cliente($pdo);
     }
-    
-}
 
+    public function index(){
+       return $this->cliente->read(); 
+    }
+
+    public function create($name,$email,$cpf){
+        $this->cliente->create($name,$email,$cpf);  
+    }
+    public function update ($id,$name,$email,$cpf){
+        $this->cliente->update($id,$name,$email,$cpf);
+    }
+    public function delete ($id){
+        $this->cliente->delete($id);
+    }
+
+}
 ?>
